147597cbe66dab53caace05ff15f4ec848b3be06
yacine <yacineaaddi@gmail.com>
2025-04-13 22:05:52 +0100

    Updating App.js File

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ function App() {
   console.log(noteList);
   console.log(typeof noteList);
   return (
-      <div className="px-12">
-        <div className="flex flex-col items-center justify-center py-8 gap-4">
-          <h1 className="text-xl text-semibold">To Do Tasks</h1>
-          <Input noteList={noteList} setNoteList={setNoteList} />
-        </div>
-        <div className=" flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12">
-          {/* */}
-        </div>
+    <div className="px-12">
+      <div className="flex flex-col items-center justify-center py-8 gap-4">
+        <h1 className="text-xl text-semibold">To Do Tasks</h1>
+        <Input noteList={noteList} setNoteList={setNoteList} />
       </div>
-    );
-  
+      <div className=" flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12">
+        {noteList.map((Note, index) => (
+          <Board
+            key={index}
+            index={index}
+            Note={Note}
+            noteList={noteList}
+            setNoteList={setNoteList}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
