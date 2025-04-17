25f3dff7c23621260c4bf00280e5931ebcff16d8
yacine <yacineaaddi@gmail.com>
2025-04-17 12:26:23 +0100

    Updating Board.jsx File

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -11,7 +11,17 @@ const Board = ({ Note, index, noteList, setNoteList }) => {
   };
   return (
     <>
-
+      <div className="max-w-md rounded-xl flex flex-col items-center justify-start border text-center text-lg pt-3 pb-4 px-4">
+        <p>{Note}</p>
+        <button
+          onClick={DeleteNote}
+          className="bg-red-500 text-white rounded-lg py-1 px-2 mt-4"
+        >
+          Delete
+        </button>
+      </div>
     </>
   );
 };
+
+export default Board;
