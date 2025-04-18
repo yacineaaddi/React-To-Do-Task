01b4d7aa8ea004805e60fb183b22119db1208aa5
yacine <yacineaaddi@gmail.com>
2025-04-18 22:39:31 +0100

    Updating input.jsx File

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,2 +1,18 @@
 import { useState } from "react";
 
+const Input = ({ noteList, setNoteList }) => {
+  const [input, setInput] = useState("");
+  const handleAddNote = (e) => {
+    e.preventDefault();
+    setNoteList([...noteList, input]);
+    setInput("");
+    
+  };
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Input;
