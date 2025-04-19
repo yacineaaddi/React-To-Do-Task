54f92fe5e35ee1d0ee9695914866714785ac444c
yacine <yacineaaddi@gmail.com>
2025-04-19 17:04:34 +0100

    Updating input.jsx File

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -10,7 +10,15 @@ const Input = ({ noteList, setNoteList }) => {
   };
   return (
     <>
-
+      <form className="flex flex-row items-center gap-3">
+        <input className="border rounded-lg py-1.5 px-2.5 px-2"
+          type="text"
+          placeholder="add a task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <button className="bg-violet-400 text-white py-1.5 px-3 rounded-lg hover:opacity-70 font-semibold" onClick={handleAddNote}>ADD</button>
+      </form>
     </>
   );
 };
